8cb10c3d0fced051fc17c585885d434f9d24476e
Clint Ruoho <clint@wtfismyip.com>
2022-04-06 15:38:03 +0000

    Remove old Debian build images

--- unbound/unbound.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-/usr/lib/unbound/package-helper chroot_setup
-/usr/lib/unbound/package-helper root_trust_anchor_update
-CPUS=`lscpu | grep ^CPU\(s | cut -d : -f2 | sed "s/^ *//g"`
-sed -i "s/CPUS/$CPUS/g" /etc/unbound/unbound.conf
-/usr/sbin/unbound -d
